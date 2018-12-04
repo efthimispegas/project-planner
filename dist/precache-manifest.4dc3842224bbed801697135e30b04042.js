e4b94be66e0955897046a68042e93274328bb0f9
efthimispegas <efthimispegas@gmail.com>
2018-12-04 17:41:56 +0200

    Production build and final touches

---
+++ dist/precache-manifest.4dc3842224bbed801697135e30b04042.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "82a1a19bf7c3ec70ee39",
+    "url": "/static/js/main.82a1a19b.chunk.js"
+  },
+  {
+    "revision": "b9a6ff4c042b0101130d",
+    "url": "/static/js/1.b9a6ff4c.chunk.js"
+  },
+  {
+    "revision": "82a1a19bf7c3ec70ee39",
+    "url": "/static/css/main.c20bb83e.chunk.css"
+  },
+  {
+    "revision": "d26aa7a5399861f23b6f28b2512ad674",
+    "url": "/index.html"
+  }
+];
